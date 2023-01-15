a0e9b91d57bfae7f62d3442b6c48ac7a28d18972
benyaminrmb <benyaminrmb@gmail.com>
2023-01-15 07:24:36 +0330

    added paginate and fix some design

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -14,7 +14,7 @@ import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 import 'swiper/css/scrollbar'
 import 'swiper/css/autoplay'
-import {useGetPosts} from "~/composables/admin/post/useGetPosts";
+import {useGetPosts} from '~/composables/admin/post/useGetPosts'
 
 const route = useRoute()
 const router = useRouter()
@@ -22,129 +22,116 @@ const nuxtApp = useNuxtApp()
 
 let page = ref(route.query.page ? route.query.page : 1)
 
-let index_data = await useGetIndexPosts(page)
-let banner_posts = index_data.data.bannerPosts;
-let main_posts = index_data.data.indexPosts;
-const changePage = async (new_page: any) => {
-    router.push({
-        query: {page: new_page},
-    })
-    index_data = await useGetIndexPosts(new_page)
-    banner_posts = index_data.data.bannerPosts;
-    main_posts = index_data.data.indexPosts;
-    document.body.scrollIntoView({behavior: 'smooth', block: 'start'});
-
+let request_data = await useGetIndexPosts(page)
+let banner_posts = request_data.data.bannerPosts
+let main_posts = request_data.data.indexPosts
+const changePage = async (new_page: number) => {
+    if (new_page > 1) {
+        router.push({
+            query: {page: new_page},
+        })
+    }else{
+        router.push({
+            query: null,
+        })
+    }
+    request_data = await useGetIndexPosts(new_page)
+    banner_posts = request_data.data.bannerPosts
+    main_posts = request_data.data.indexPosts
 }
 
-
-
-const modules = [Pagination, Autoplay]
-const options = {
-  slidesPerView: 'auto',
-  spaceBetween: 10,
-}
 </script>
 <template>
-    pppp {{ page }}
-    <div class="grid grid-cols-12 w-full gap-3">
-        <div class="flex flex-wrap col-span-3">3</div>
-        <div class="flex flex-wrap col-span-6 gap-6">
-            <div class="flex flex-wrap w-full dir-ltr">
-                <swiper
-                    class="rounded-3xl w-full"
-                    :slides-per-view="1"
-                    :space-between="50"
-                    :modules="modules"
-                    :options="options"
-                    :loop="true"
-                    :pagination="{ clickable: true, dynamicBullets: true }"
-                >
-                    <swiper-slide
-                        class="w-full"
-                        :virtualIndex="index"
-                        v-for="(post, index) in banner_posts"
-                        :key="index"
-                    >
-                        <div class="flex w-full flex-wrap">
-                            <div
-                                class="flex rounded-3xl bg-center bg-cover bg-no-repeat bg-sca flex-wrap w-full h-[320px]"
-                                :style="`background-image:url(${post.image.large})`"
-                            >
-                                <div
-                                    class="absolute items-stretch dir-rtl grid grid-cols-6 w-full h-full bottom-0 right-0 bg-gradient-to-l from-base-100/90 p-3"
-                                >
-                                    <div
-                                        class="flex flex-wrap gap-3 col-span-3"
-                                    >
-                                        <h2
-                                            class="flex w-full text-right flex-wrap content-color text-3xl leading-10"
-                                        >
-                                            {{ post.title }}
-                                        </h2>
-                                        <div class="flex w-full flex-wrap">
-                                            <p
-                                                class="content-color truncate text-ellipsis"
-                                            >
-                                                {{ post.text }}
-                                            </p>
-                                        </div>
-                                        <div
-                                            class="flex self-end w-full flex-wrap"
-                                        >
-                                            <div
-                                                class="flex rounded-2xl text-primary-content items-center gap-2 border-1 border-primary/60 bg-primary p-1"
-                                            >
-                                                <span class="text-xs">
-                                                    {{ post.created_at.shamsi }}
-                                                </span>
-                                                <div
-                                                    class="flex rounded-2xl text-primary-content bg-primary-focus p-2 hover:shadow hover:-translate-y-[3px] ease-in transition"
-                                                >
-                                                    <span
-                                                        class="dir-ltr text-sm font-bold"
-                                                    >
-                                                        @{{
-                                                            post.source.source
-                                                                .name
-                                                        }}
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </swiper-slide>
-                </swiper>
-            </div>
-
-            <div class="flex flex-wrap w-full px-3">
-                <span class="text-3xl font-bold content-color"
-                    >آخرین اخبار</span
-                >
-            </div>
-
-            <div v-if="main_posts.data" class="grid grid-cols-2 w-full gap-4">
-                <post-item
-                    v-for="(post, index) in main_posts.data"
-                    :item="post"
-                    :key="index"
-                />
-                <div class="flex flex-wrap w-full justify-center">
-                    <div class="btn-group">
-                        <button
-                            v-for="page in main_posts.links"
-                            v-html="page.label"
-                            @click="changePage(page.label)"
-                            :disabled="page.active || !page.url"
-                            class="btn"></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-wrap col-span-3">3</div>
+  <div class="flex flex-wrap w-full justify-center">
+    <div class="btn-group">
+      <button
+        v-for="page in main_posts.links"
+        v-html="page.label"
+        @click="changePage(page.label)"
+        :disabled="page.active || !page.url"
+        class="btn"></button>
     </div>
+  </div>
+
+
+  <div class="grid grid-cols-12 w-full gap-3">
+    <div class="flex flex-wrap col-span-3">3</div>
+    <div class="flex flex-wrap col-span-6 gap-6">
+      <div class="flex flex-wrap w-full dir-ltr">
+        <swiper
+          class="rounded-3xl w-full"
+          :slides-per-view="1"
+          :space-between="50"
+          :modules="modules"
+          :options="options"
+          :loop="true"
+          :pagination="{clickable: true, dynamicBullets: true}">
+          <swiper-slide
+            class="w-full"
+            :virtualIndex="index"
+            v-for="(post, index) in banner_posts"
+            :key="index">
+            <div class="flex w-full flex-wrap">
+              <div
+                class="flex rounded-3xl bg-center bg-cover bg-no-repeat bg-sca flex-wrap w-full h-[320px]"
+                :style="`background-image:url(${post.image.large})`">
+                <div
+                  class="absolute items-stretch dir-rtl grid grid-cols-6 w-full h-full bottom-0 right-0 bg-gradient-to-l from-base-100/90 p-3">
+                  <div class="flex flex-wrap gap-3 col-span-3">
+                    <h2
+                      class="flex w-full text-right flex-wrap content-color text-3xl leading-10">
+                      {{ post.title }}
+                    </h2>
+                    <div class="flex w-full flex-wrap">
+                      <p class="content-color truncate text-ellipsis">
+                        {{ post.text }}
+                      </p>
+                    </div>
+                    <div class="flex self-end w-full flex-wrap">
+                      <div
+                        class="flex rounded-2xl text-primary-content items-center gap-2 border-1 border-primary/60 bg-primary p-1">
+                        <span class="text-xs">
+                          {{ post.created_at.shamsi }}
+                        </span>
+                        <div
+                          class="flex rounded-2xl text-primary-content bg-primary-focus p-2 hover:shadow hover:-translate-y-[3px] ease-in transition">
+                          <span class="dir-ltr text-sm font-bold">
+                            @{{ post.source.source.name }}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+
+      <div class="flex flex-wrap w-full px-3">
+        <span class="text-3xl font-bold content-color">آخرین اخبار</span>
+      </div>
+
+      <div v-if="main_posts.data" class="grid grid-cols-2 w-full gap-4">
+        <post-item
+          v-for="(post, index) in main_posts.data"
+          :item="post"
+          :key="index" />
+        <div class="flex flex-wrap w-full justify-center">
+          <div class="btn-group">
+            <button
+              v-for="page in main_posts.links"
+              v-html="page.label"
+              @click="changePage(page.label)"
+              :disabled="page.active || !page.url"
+              class="btn"></button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-wrap col-span-3">3</div>
+  </div>
 </template>
 <style>
 .swiper-pagination-bullet-active {
