71d92fe9976afe14f0cec40df4d78b5498a93868
benyaminrmb <benyaminrmb@gmail.com>
2023-02-17 21:57:57 +0330

    changed dark theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,50 @@
-import { Config } from "tailwindcss";
+import {Config} from 'tailwindcss'
 export default <Config>{
-    darkMode: ["class"],
-    /* Extend the Tailwind config here */
-    content: [
-        "./client/content/**/**.md",
-        "./client/assets/**/*.css",
-        "./client/components/*.{vue,js}",
-        "./client/components/**/*.{vue,js}",
-        "./client/pages/*.vue",
-        "./client/pages/**/*.vue",
-        "./client/plugins/**/*.{js,ts}",
-        "./client/*.{vue,js,ts}",
+  darkMode: ['class'],
+  /* Extend the Tailwind config here */
+  content: [
+    './client/content/**/**.md',
+    './client/assets/**/*.css',
+    './client/components/*.{vue,js}',
+    './client/components/**/*.{vue,js}',
+    './client/pages/*.vue',
+    './client/pages/**/*.vue',
+    './client/plugins/**/*.{js,ts}',
+    './client/*.{vue,js,ts}',
+  ],
+  daisyui: {
+    rtl: true,
+    themes: [
+      {
+        dark: {
+          primary: '#FCA311',
+          secondary: '#14213d',
+          accent: '#c9c9ee',
+          neutral: '#1f2937',
+          'base-100': '#0a111f',
+          info: '#e76f51',
+          success: '#10b981',
+          warning: '#86198f',
+          error: '#e63946',
+        },
+        light: {
+          primary: '#7c3aed',
+          secondary: '#D926A9',
+          accent: '#1FB2A6',
+          neutral: '#191D24',
+          'base-100': '#f3f4f6',
+          'base-50': '#f5f5f6',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
     ],
-    daisyui: {
-        rtl: true,
-        themes: [
-            {
-                dark: {
-                    primary: "#7c3aed",
-                    secondary: "#ec4899",
-                    accent: "#F3CC30",
-                    neutral: "#20134E",
-                    "base-100": "#28264e",
-                    "base-50": "#37346a",
-                    info: "#53C0F3",
-                    success: "#71EAD2",
-                    warning: "#F3CC30",
-                    error: "#E24056",
-                },
-                light: {
-                    primary: "#7c3aed",
-                    secondary: "#D926A9",
-                    accent: "#1FB2A6",
-                    neutral: "#191D24",
-                    "base-100": "#f3f4f6",
-                    "base-50": "#f5f5f6",
-                    info: "#3ABFF8",
-                    success: "#36D399",
-                    warning: "#FBBD23",
-                    error: "#F87272",
-                },
-            },
-        ],
-    },
-    plugins: [
-        require("daisyui"),
-        require("@tailwindcss/line-clamp"),
-        // ...
-    ],
-};
+  },
+  plugins: [
+    require('daisyui'),
+    require('@tailwindcss/line-clamp'),
+    // ...
+  ],
+}
