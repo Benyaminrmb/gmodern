00862b00d2055020d898106645d237cdde7cb892
benyaminrmb <benyaminrmb@gmail.com>
2023-02-17 21:56:50 +0330

    changed main page style

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -21,42 +21,37 @@ const router = useRouter()
 const nuxtApp = useNuxtApp()
 
 let page = ref(route.query.page ? route.query.page : 1)
+let posts_loading = ref(false)
 
-let request_data = await useGetIndexPosts(page)
+let request_data = await useGetIndexPosts(page.value)
+
 let banner_posts = request_data.data.bannerPosts
 let main_posts = request_data.data.indexPosts
 const changePage = async (new_page: number) => {
-    if (new_page > 1) {
-        router.push({
-            query: {page: new_page},
-        })
-    }else{
-        router.push({
-            query: null,
-        })
-    }
-    request_data = await useGetIndexPosts(new_page)
-    banner_posts = request_data.data.bannerPosts
-    main_posts = request_data.data.indexPosts
+  window.scrollTo(0, 0)
+  posts_loading.value = true
+  if (new_page > 1) {
+    router.push({
+      query: {page: new_page},
+    })
+  } else {
+    router.push({
+      query: null,
+    })
+  }
+  request_data = await useGetIndexPosts(new_page)
+  banner_posts = request_data.data.bannerPosts
+  main_posts = request_data.data.indexPosts
+  posts_loading.value = false
 }
-
+posts_loading.value = false
 </script>
 <template>
-  <div class="flex flex-wrap w-full justify-center">
-    <div class="btn-group">
-      <button
-        v-for="page in main_posts.links"
-        v-html="page.label"
-        @click="changePage(page.label)"
-        :disabled="page.active || !page.url"
-        class="btn"></button>
-    </div>
-  </div>
-
+  {{ posts_loading }}
 
   <div class="grid grid-cols-12 w-full gap-3">
-    <div class="flex flex-wrap col-span-3">3</div>
-    <div class="flex flex-wrap col-span-6 gap-6">
+    <div class="flex flex-wrap col-span-3"></div>
+    <div class="flex flex-wrap col-span-12 md:col-span-6 p-4 gap-6">
       <div class="flex flex-wrap w-full dir-ltr">
         <swiper
           class="rounded-3xl w-full"
@@ -71,9 +66,9 @@ const changePage = async (new_page: number) => {
             :virtualIndex="index"
             v-for="(post, index) in banner_posts"
             :key="index">
-            <div class="flex w-full flex-wrap">
+            <nuxt-link :to="`/post/${post.id}`" class="flex w-full flex-wrap">
               <div
-                class="flex rounded-3xl bg-center bg-cover bg-no-repeat bg-sca flex-wrap w-full h-[320px]"
+                class="flex rounded-3xl bg-origin-content bg-center bg-contain bg-no-repeat bg-sca flex-wrap w-full h-[320px]"
                 :style="`background-image:url(${post.image.large})`">
                 <div
                   class="absolute items-stretch dir-rtl grid grid-cols-6 w-full h-full bottom-0 right-0 bg-gradient-to-l from-base-100/90 p-3">
@@ -104,7 +99,7 @@ const changePage = async (new_page: number) => {
                   </div>
                 </div>
               </div>
-            </div>
+            </nuxt-link>
           </swiper-slide>
         </swiper>
       </div>
@@ -113,24 +108,32 @@ const changePage = async (new_page: number) => {
         <span class="text-3xl font-bold content-color">آخرین اخبار</span>
       </div>
 
-      <div v-if="main_posts.data" class="grid grid-cols-2 w-full gap-4">
-        <post-item
-          v-for="(post, index) in main_posts.data"
-          :item="post"
-          :key="index" />
-        <div class="flex flex-wrap w-full justify-center">
-          <div class="btn-group">
+      <div
+        v-if="main_posts.data"
+        class="grid grid-cols-1 md:grid-cols-2 w-full gap-4">
+        <template v-if="posts_loading">
+          <post-loading v-for="(i, index) in 20" :item="i" :key="index" />
+        </template>
+        <template v-else>
+          <post-item
+            v-for="(post, index) in main_posts.data"
+            :item="post"
+            :key="index" />
+        </template>
+      </div>
+      <nav class="flex flex-wrap w-full justify-center">
+        <ul class="flex flex-wrap items-center -space-x-px">
+          <li v-for="page in main_posts.links">
             <button
-              v-for="page in main_posts.links"
               v-html="page.label"
               @click="changePage(page.label)"
               :disabled="page.active || !page.url"
-              class="btn"></button>
-          </div>
-        </div>
-      </div>
+              class="px-3 py-2 leading-tight border border-secondary disabled:bg-secondary-focus hover:bg-accent-focus hover:text-gray-700"></button>
+          </li>
+        </ul>
+      </nav>
     </div>
-    <div class="flex flex-wrap col-span-3">3</div>
+    <div class="flex flex-wrap col-span-3"></div>
   </div>
 </template>
 <style>
