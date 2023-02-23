0640664d504d096a986fb166517e8d7e334cec7b
benyaminrmb <benyaminrmb@gmail.com>
2023-02-23 08:07:32 +0330

    add any

--- client/middleware/admin.ts
+++ client/middleware/admin.ts
@@ -2,7 +2,7 @@ import {useAuth} from "~/stores/auth";
 
 // @ts-ignore
 export default defineNuxtRouteMiddleware(async (to, from,nuxtApp ) => {
-    const use_auth = useAuth()
+    const use_auth:any = useAuth()
     // @ts-ignore
     console.log('use_auth.user.is_admin',use_auth.user.is_admin)
     if(!use_auth.user.is_admin){
