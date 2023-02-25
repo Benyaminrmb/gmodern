58c8fff5dc3763559d8f1b03a3752ea1787e4928
benyaminrmb <benyaminrmb@gmail.com>
2023-02-25 07:26:59 +0330

    remove spaces

--- client/pages/admin/crawl/index.vue
+++ client/pages/admin/crawl/index.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
-
-
-
-import {useRunCrawler} from "~/composables/admin/crawl/useRunCrawler";
+import {useRunCrawler} from '~/composables/admin/crawl/useRunCrawler'
 
 definePageMeta({
   layout: 'admin',
@@ -11,21 +8,24 @@ definePageMeta({
 useHead({
   title: 'خزش -',
 })
-let crawl_loading=ref(false)
-let crawl_result=null
-const runCrawler =async ()=>{
-    crawl_loading=ref(true)
-    crawl_result = await useRunCrawler()
-    crawl_loading=ref(false)
+let crawl_loading = ref(false)
+let crawl_result = null
+const runCrawler = async () => {
+  crawl_loading = await ref(true)
+  crawl_result = await useRunCrawler()
+  crawl_loading = await ref(false)
 }
 </script>
 
 <template>
-    <div class="flex flex-wrap">
-        {{ crawl_loading }}
-        <button @click="runCrawler"
-                :disabled="crawl_loading"
-            class="btn btn-primary">شروع خزش</button>
-        {{ crawl_result }}
-    </div>
+  <div class="flex flex-wrap">
+    {{ crawl_loading }}
+    <button
+      @click="runCrawler"
+      :disabled="crawl_loading"
+      class="btn btn-primary">
+      شروع خزش
+    </button>
+    {{ crawl_result }}
+  </div>
 </template>
